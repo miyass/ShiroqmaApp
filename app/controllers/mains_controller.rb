1d716e9214142c0be1ed5d12e73710d9b0d7ef49
vencha <exe.miyakura0629@gmail.com>
2017-02-18 16:12:11 +0900

    controller部分追加

--- app/controllers/mains_controller.rb
+++ app/controllers/mains_controller.rb
@@ -1,11 +1,40 @@
 class MainsController < ApplicationController
 
     def top
-        
+        @form = Form.all
     end
 
     def form
-    
+        
+    end
+
+    def create
+        Form.create(form_params)
+    end
+
+    def feed
+        @form_feed = Form.all.order("id ASC")
+    end
+
+    def picture
+        @content_pict = Form.select("picture")
+    end
+
+    def illust
+        @content_illust = Form.select("illust")
+    end
+
+    def music
+        @content_music = Form.select("music")
+    end
+
+    def movie
+        @content_movie = Form.select("movie")
+    end
+
+    private
+    def form_params
+        params.permit(:text, :picture, :illust)
     end
 
 end
